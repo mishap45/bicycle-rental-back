75b5c2e857996f291d6b3d2f9e2df242254317b6
mishap45 <pavlovmisha45@gmail.com>
2021-03-21 10:47:36 +0200

    created graphql interface in browser, added query param.

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const koa = require('koa');
+const graphqlHTTP = require('koa-graphql');
+const mount = require('koa-mount');
+const schema = require('./graphql/schema');
+const mongoose = require('mongoose');
+const root = require('./graphql/root');
+
+mongoose.connect(`mongodb://localhost/calendar`, {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Database connected.'));
+
+const app = new koa();
+app.listen(9000);
+
+app.on('error', err => {
+    console.log('Server error', err);
+});
+
+app.use(
+    mount(
+        '/graphql',
+        graphqlHTTP({
+            schema,
+            rootValue: root,
+            graphiql: true
+        })
+    )
+);
